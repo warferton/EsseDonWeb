4f53103d6a560ec709399d5f87a24ae17bc6446f
titimiti <ikhozhaynov@mail.ru>
2021-06-30 23:51:50 +0300

    Added and styled footer, changed background color for header

---
+++ front/components/footer/footer.component.tsx
@@ -0,0 +1,16 @@
+import styles from '../../styles/Footer.module.css';
+
+export function Footer() {
+    return(
+        <div className = { styles.footerBackground }>
+            <p className = { styles.infoText }>
+                Ростов-на-Дону
+                <br>Красноармейская 166</br>
+                ‎<br>+7 863 310 41 10</br>
+            </p>
+            <h1 className = { styles.motoText}>
+                Jazz lives here <span className = { styles.motoDot}>.</span>
+            </h1>
+        </div>
+    );
+}
