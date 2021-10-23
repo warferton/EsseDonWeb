a4516d00d61ac82e600845b296d80cb32207acb3
titimiti <ikhozhaynov@mail.ru>
2021-10-23 17:51:07 +0300

    chenged secondGroupEvents title

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -71,7 +71,7 @@ export default function Home({ mainGroupEvents, secondGroupEvents, generalGroupE
 
             { secondGroupEvents.length > 0 &&
               <Typography className = { styles.heading }>
-                Лучшее на этой неделе
+                В ближайшие дни
               </Typography>
             }
             
